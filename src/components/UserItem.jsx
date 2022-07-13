227bb1282db0e8cdb482191af04aab5868b895b0
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-07-14 01:04:00 +0300

    complete add Imagine

--- src/components/UserItem.jsx
+++ src/components/UserItem.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import NoUserIcon from '../assets/images/no-user-icon.png';
 
 
+
 function UserItem(props) {
     const {name, email, isGoldClient, salariu, imagine} = props;
-
+    
     return (
         <div>
             <h3>{ name }</h3>
@@ -16,20 +17,17 @@ function UserItem(props) {
             <p><b>salariu:</b> { salariu === undefined ? 'sumă nespecificată' : salariu }</p>
             <p><b>imagine:</b> { imagine === undefined ? 'imagine nespecificată' : imagine }</p>
             <img
-                src={NoUserIcon}
-                alt="car"
+                onError={(event) => (event.target.src=NoUserIcon) }
+                src={ imagine === undefined ? NoUserIcon : imagine }
+                alt="alt text"
             />
         </div>
     );
 }
 
 
-const isImgLink = (url) => {
-    if (typeof url !== 'string') {
-      return false;
-    }
-    return (url.match(/^http[^\?]*.(jpg|jpeg|gif|png|tiff|bmp)(\?(.*))?$/gmi) !== null);
-  }
+
+
 
   
 
