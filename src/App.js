903d76015926ca6304e020f8b21da37ce508554b
Adrian Gheorghe <adrian.gh1989@gmail.com>
2022-07-14 09:44:20 +0300

    add font color btn

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends React.Component {
     super();
     this.state = {
       background: 'white',
+      color: 'black',
       users: []
     };
   }
@@ -60,6 +61,12 @@ class App extends React.Component {
     event.target.reset();
   }
 
+  handleColorChange(event){
+    console.log(event.target.value);
+    this.setState({color: event.target.value})
+  }
+  
+
   logAllUsersData(){
     this.state.users.map((user, index)=>{
       console.log(index);
@@ -74,11 +81,14 @@ class App extends React.Component {
 
   render() {
     return(
-      <div className="app" style={{background: this.state.background}}>
+      <div className="app" style={{background: this.state.background, color: this.state.color}}>
         <h1>Admin panel - Proiectul 1</h1>
         {/* agh start */}
         <button onClick={()=>{this.logAllUsersData()}}>App.js - Log all users</button>
         {/* agh end */}
+        <br/><br/>
+        <label>Font Color: </label>
+        <input id="FontColor" type="color" onChange={(event)=>this.handleColorChange(event)}></input>
         <UserAddForm submitAddForm={(event, name, email, isGoldClient, salariu, imagine) => this.submitAddForm(event, name, email, isGoldClient, salariu, imagine)}/>
         <UserList users={this.state.users}/>
         <input type="color" onChange={(event) => this.changeColor(event)}/>
